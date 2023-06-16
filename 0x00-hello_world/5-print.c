8b46f652cafad36083b2c36f922cb194d9f272da
victornwakpa <nwakpavictor@gmail.com>
2023-06-15 18:56:10 -0700

    added some content to 5-print.c file

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This will contain the code to be executed
+ * Return: This will return 0 if successfully, and 1 if not successfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
