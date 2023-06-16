e749e4c537bd761461c3856683a55e5ee3fee2cf
victornwakpa <nwakpavictor@gmail.com>
2023-06-15 18:24:11 -0700

    added some contents to 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  A This the name of the function to houses the code
+ * Return: This will return 0 if correct and return 1 if the code fails
+ */
+void main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
