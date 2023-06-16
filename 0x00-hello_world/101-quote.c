0c0fa2915aa3890a04403dcaa284cd0f5f383cea
victornwakpa <nwakpavictor@gmail.com>
2023-06-15 21:20:45 -0700

    modified 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - This is the main function
  * Description: This is a program that prints so text and sandard error
- * Return: Print out the standard erro
+ * Return: Print out the standard error
  */
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	return (1);
 }
