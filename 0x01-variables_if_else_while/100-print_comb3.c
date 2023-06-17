adaf1dac09e4fe25c83c6c0872965bdfa63de420
victornwakpa <nwakpavictor@gmail.com>
2023-06-16 21:59:06 -0700

    new advanced file added

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 9)
+	{
+		j = i + 1;
+		while (j <= 9)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (!(i == 8 && j == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
