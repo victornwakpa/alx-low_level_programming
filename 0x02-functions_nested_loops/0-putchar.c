fc685a10fc0b2b4a28999b82cf90aa2ab2a92523
victornwakpa <nwakpavictor@gmail.com>
2023-06-21 04:13:31 -0700

    added a new file called 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include main.h
+/**
+ * main - the main function for the program
+ * Description: This programs prints _putchar
+ * Return: O if successfully
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
