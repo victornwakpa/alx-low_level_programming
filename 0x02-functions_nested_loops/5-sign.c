c9510cddd71891575781128e3a0d5808a1b31e74
victornwakpa <nwakpavictor@gmail.com>
2023-06-21 08:08:44 -0700

    new file for task 5 added

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - main function
+ * Description: A function that print the sign of a number
+ * @n: the interger
+ * Return: return values are 1, 0, -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+}
