e29fbe58a97cc220aeca3c03f2a9a84f981daf30
victornwakpa <nwakpavictor@gmail.com>
2023-06-21 09:31:22 -0700

    hello

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - main function
+ * Return: -num, num
+ */
+
+int _abs(int num) {
+    if (num < 0) {
+        return -num;
+    } else {
+        return num;
+    }
+}
