7f3cdabd51fdaf7e4e33aa5cf27b63c5c5d40662
victornwakpa <nwakpavictor@gmail.com>
2023-06-22 10:29:59 -0700

    Added a new file for task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isdigit - the main function for this program
+ * Return: This returns 1 if c is a digit
+ * and 0 if otherwise
+ * @c: The subject
+ */
+
+int _isdigit(int c);
+{
+	if(c >= '0' && c <= '0' )
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
