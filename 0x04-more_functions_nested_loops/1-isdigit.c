46d06c1056f3a05472bdc15444eca97501a43a9e
victornwakpa <nwakpavictor@gmail.com>
2023-06-22 11:53:16 -0700

    hello

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,6 @@
 /**
  * _isdigit - the main function for this program
  * Description: A function that checks if c is a digit
- * @c: The subject
  * Return: This returns 1 if c is a digit and 0 if otherwise
  */
 
