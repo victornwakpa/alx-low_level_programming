f2d7d9f63bac72d2e339a082bdee5333b45f87a3
victornwakpa <nwakpavictor@gmail.com>
2023-06-22 10:57:27 -0700

    added a file for task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - the main function
+ * Description: this functions multiples to intergers
+ * @a: the first value
+ * @b: second value
+ * Return: value to main
+ */
+
+int mul(int a, int b)
+{
+	int multiply;
+
+	multiply = a * b;
+	return (multiply);
+}
