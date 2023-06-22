4cb8d64819bcf12661ec299c7117dc07ebc955c0
victornwakpa <nwakpavictor@gmail.com>
2023-06-22 10:44:14 -0700

    hello

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,9 +2,8 @@
 #include "main.h"
 /**
  * _isdigit - the main function for this program
- * Return: This returns 1 if c is a digit
- * and 0 if otherwise
- * @c: The subject
+ * @c: The subbject
+ * Return: This returns 1 if c is a digit and 0 if otherwise
  */
 
 int _isdigit(int c)
