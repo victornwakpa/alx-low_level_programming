cbbddf3c4ac649ef6f8a06c3daa13f1ec690241f
victornwakpa <nwakpavictor@gmail.com>
2023-06-23 03:05:56 -0700

    added 0-upper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isupper - the ain function for this program
+ * Description: hello
+ * @c: the return value
+ * Return: method for the program
+ */
+
+int _isupper(int c)
+{
+	int reault;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		result = 1;
+	}
+	else
+	{
+		result = 0;
+	}
+	return (result);
+}
