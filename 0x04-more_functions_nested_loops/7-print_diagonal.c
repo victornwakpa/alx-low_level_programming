87e0f9ee0d46d231853f519dc81691055dae7e21
victornwakpa <nwakpavictor@gmail.com>
2023-06-23 21:30:32 -0700

    added some content to task 7 file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - function of the program
+ * @n: return value
+ * Return: no of lines
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
