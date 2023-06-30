6c7334156d0b11d5e365f04034ff78893dee4945
victornwakpa <nwakpavictor@gmail.com>
2023-06-30 03:40:17 -0700

    added comtent to 1-swap.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_88 - the function for the program
+ */
+
+void reset_to_98(int *n)
+{
+
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - function for the program
+ * @*a: first parameter
+ * @*b: second parameter
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
